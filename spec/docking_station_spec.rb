d1b8b3487fa6486b60269690eea6bdb0169f8b03
B-raw <michaelbarry1989@hotmail.com>
2016-09-27 15:52:25 +0100

    Add docking station full test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -37,5 +37,13 @@ describe DockingStation do
     it 'raises an error when no bikes' do
       expect{ subject.release_bike }.to raise_error 'No bikes available'
     end
-  end	
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock Bike.new}.to raise_error 'Docking station full'
+    end
+  end
+
 end
