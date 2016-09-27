f10db2b8b44300ea84c27f3e8182f3c65caa0ad7
James <James.Steel@me.com>
2016-09-27 16:50:09 +0100

    added init and array for bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 
+  it { is_expected.to respond_to :release_bike}
+
   describe '#release_bike' do
 
     it 'releases a bike' do
@@ -20,32 +22,28 @@ describe DockingStation do
 
    describe '#dock' do
      it 'raises an error when there is a bike in that docking station' do
-       subject.dock(Bike.new)
+       20.times { subject.dock(Bike.new) }
        expect { subject.dock Bike.new }.to raise_error 'Docking station full'
      end
    end
-=begin
+
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
     expect(bike).to be_working
   end
-=end
-  it 'docks something' do
+
+   it 'docks something' do
     bike = Bike.new
     #return the bike we dock back
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
 
   end
 
-  it 'return docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    # we need to return the bike we just docked
-    expect(subject.bike).to eq bike
-  end
 
-  it {is_expected.to respond_to :release_bike}
+
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
 
 end
