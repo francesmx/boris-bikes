8eb226f3544975cee99e8be5168afa13f851f934
Frances Maxwell <fmaxwell@gmail.com>
2016-09-27 15:45:20 +0100

    Finishing exercise 12.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,17 @@ require 'docking_station'
 describe DockingStation do
 
   describe '#release_bike' do
+
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+    
   end
 =begin
   it 'releases working bikes' do
