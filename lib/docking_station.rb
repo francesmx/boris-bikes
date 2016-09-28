fb3b2c7741360bd6859aef3b6b82a145d68c7df5
B-raw <michaelbarry1989@hotmail.com>
2016-09-28 10:39:16 +0100

    Finish 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,13 @@
 require_relative 'bike'
 
+
+
 class DockingStation
   #attr_reader :bikes
   attr_reader :capacity
   DEFAULT_CAPACITY = 20
 
-  def initialize(capacity==DEFAULT_CAPACITY)
+  def initialize(capacity=DEFAULT_CAPACITY)
     @bikes = []
     @capacity = capacity
   end
