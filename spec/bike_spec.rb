fdded2919a379d00632ed9c90386a811db95e42e
Frances Maxwell <fmaxwell@gmail.com>
2016-09-28 10:56:12 +0100

    Wrote failing test for ex. 18 part 1.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,3 +3,10 @@ require 'bike'
 describe Bike do
   it {is_expected.to respond_to :working?}
 end
+
+describe '#report_broken' do
+  it 'allows the user to report that a bike is broken' do
+    subject.report_broken(Bike.new)
+    expect { subject.working? == false }
+  end
+end
